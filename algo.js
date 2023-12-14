0ded3bd31fc2af47e14ff64e15c3cd4ca8e8c866
hafsa-CH98 <chidmihafsa4@gmail.com>
2023-12-14 16:40:45 +0100

    algorithme javascript

---
+++ algo.js
@@ -0,0 +1,63 @@
+// ADDITION
+function add(num1, num2) {
+    let total = num1 + num2;
+    return total;
+}
+
+console.log(add(7, 4))
+
+
+
+
+// MULTIPLICATION
+function multiply(num1, num2) {
+    let product = num1 * num2;
+    return product;
+}
+
+console.log(multiply(9, 5));
+
+
+// MODULO
+function modulo(num1, num2) {
+    let total = num1 % num2;
+    return total;
+}
+
+console.log(modulo(9,5));
+
+
+
+
+// SOUSTRACTION
+function soustrac(num1, num2) {
+    let total = num1 - num2;
+    return total;
+}
+
+console.log(soustrac(10,2));
+
+
+
+
+// DIVISION
+function division(num1, num2) {
+    let total = num1 / num2;
+    return total;
+}
+
+console.log(division(7, 6))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
