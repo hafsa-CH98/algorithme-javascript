4393d6e289396acfefcb26fe5c7321df6941243c
hafsa-CH98 <chidmihafsa4@gmail.com>
2023-12-14 17:41:12 +0100

    recursivite et resolution

--- algo.js
+++ algo.js
@@ -52,7 +52,16 @@ console.log(division(7, 6))
 
 
 
+function Palindrome(test) {
+    const chaineTraitee = test.toLowerCase().replace(/\s/g, '');
 
+    return chaineTraitee === chaineTraitee.split('').reverse().join('');
+}
+
+console.log(Palindrome("gag"));
+console.log(Palindrome("kayak"));
+console.log(Palindrome("php"));
+console.log(Palindrome("radar"));
 
 
 
